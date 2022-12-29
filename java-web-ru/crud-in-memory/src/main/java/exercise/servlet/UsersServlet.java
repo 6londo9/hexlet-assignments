f984aaf2818c457b36d422bda3c41ce3df1e5cfc
@hexlet/cli <support@hexlet.io>
2022-12-29 21:02:38 +1100

    submit java-web-ru/crud-in-memory

--- java-web-ru/crud-in-memory/src/main/java/exercise/servlet/UsersServlet.java
+++ java-web-ru/crud-in-memory/src/main/java/exercise/servlet/UsersServlet.java
@@ -2,8 +2,6 @@ package exercise.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
