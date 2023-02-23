8928b7e5b8fee67547e1fc54c6f22f8486821964
@hexlet/cli <support@hexlet.io>
2023-02-23 16:21:10 +1100

    submit java-spring-ru/custom-annotations

--- java-spring-ru/custom-annotations/src/main/java/exercise/CustomBeanPostProcessor.java
+++ java-spring-ru/custom-annotations/src/main/java/exercise/CustomBeanPostProcessor.java
@@ -1,10 +1,7 @@
 package exercise;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Proxy;
 
-import exercise.calculator.Calculator;
-import exercise.calculator.CalculatorImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
@@ -17,51 +14,44 @@ import org.slf4j.LoggerFactory;
 // BEGIN
 @Component
 public class CustomBeanPostProcessor implements BeanPostProcessor {
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomBeanPostProcessor.class);
-    private final Map<Object, String> beanLoggerPair = new HashMap<>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(BeanPostProcessor.class);
+    private Map<String, Class> annotatedBeans = new HashMap<>();
+    private Map<String, String> loggingLevels = new HashMap<>();
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        for (Field field : bean.getClass().getDeclaredFields()) {
-            if (field.getAnnotation(Inspect.class) != null) {
-                beanLoggerPair.put(bean, field.getAnnotation(Inspect.class).level());
-            }
+        if (bean.getClass().isAnnotationPresent(Inspect.class)) {
+            String level = bean.getClass().getAnnotation(Inspect.class).level();
+            annotatedBeans.put(beanName, bean.getClass());
+            loggingLevels.put(beanName, level);
         }
-//        Arrays.stream(bean.getClass().getDeclaredAnnotations())
-//                .filter(field -> field.annotationType().isAnnotationPresent(Inspect.class))
-//                .map(field -> beanLoggerPair.put(bean, field.annotationType().getAnnotation(Inspect.class).level()));
-        LOGGER.info("I called from [postProcessBeforeInitialization] " + beanLoggerPair);
-        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+        return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        String message = "Was called method: {} with arguments: {}";
-        Calculator proxyInstance = (Calculator) Proxy.newProxyInstance(
-                CalculatorImpl.class.getClassLoader(),
-                CalculatorImpl.class.getInterfaces(),
+        if (!annotatedBeans.containsKey(beanName)) {
+            return bean;
+        }
+        Class beanClass = annotatedBeans.get(beanName);
+        String level = loggingLevels.get(beanName);
+
+        return Proxy.newProxyInstance(
+                beanClass.getClassLoader(),
+                beanClass.getInterfaces(),
                 (proxy, method, args) -> {
-                    String logLevel = beanLoggerPair.get(bean);
-                    switch (logLevel) {
-                        case "debug" -> {
-                            LOGGER.debug(
-                                    "Was called method: " + method.getName()
-                                    + " with arguments: " + Arrays.toString(args)
-                            );
-                            return method.invoke(bean, args);
-                        }
-                        case "info" -> {
-                            LOGGER.info(
-                                    "Was called method: " + method.getName()
-                                            + " with arguments: " + Arrays.toString(args)
-                            );
-                            return method.invoke(bean, args);
-                        }
-                        default -> throw new RuntimeException("Invalid logger level: " + logLevel);
+                    String message = String.format("Was called method: %s() with arguments: %s",
+                            method.getName(),
+                            Arrays.toString(args)
+                    );
+                    if (level.equals("info")) {
+                        LOGGER.info(message);
+                    } else {
+                        LOGGER.debug(message);
                     }
-                }
-        );
-        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+                    return method.invoke(bean, args);
+                });
+
     }
 }
 // END
--- java-spring-ru/custom-annotations/src/test/java/exercise/AppTest.java
+++ java-spring-ru/custom-annotations/src/test/java/exercise/AppTest.java
@@ -32,26 +32,26 @@ public class AppTest {
 
     @Test
     void testSum(CapturedOutput output) throws Exception {
-//        MockHttpServletResponse response = mockMvc
-//            .perform(get("/sum?a=5&b=10"))
-//            .andReturn()
-//            .getResponse();
-//
-//        assertThat(response.getStatus()).isEqualTo(200);
-//        assertThat(response.getContentAsString()).isEqualTo("15");
-//        assertThat(output).contains("Was called method: sum() with arguments: [5, 10]");
+        MockHttpServletResponse response = mockMvc
+            .perform(get("/sum?a=5&b=10"))
+            .andReturn()
+            .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getContentAsString()).isEqualTo("15");
+        assertThat(output).contains("Was called method: sum() with arguments: [5, 10]");
     }
 
     @Test
     void testMult(CapturedOutput output) throws Exception {
-//        MockHttpServletResponse response = mockMvc
-//            .perform(get("/mult?a=5&b=10"))
-//            .andReturn()
-//            .getResponse();
-//
-//        assertThat(response.getStatus()).isEqualTo(200);
-//        assertThat(response.getContentAsString()).isEqualTo("50");
-//        assertThat(output).contains("Was called method: mult() with arguments: [5, 10]");
+        MockHttpServletResponse response = mockMvc
+            .perform(get("/mult?a=5&b=10"))
+            .andReturn()
+            .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getContentAsString()).isEqualTo("50");
+        assertThat(output).contains("Was called method: mult() with arguments: [5, 10]");
     }
 
     // Проверяем, что логгирование происходит не в контролере,
@@ -59,22 +59,22 @@ public class AppTest {
     @Test
     void testContext(CapturedOutput output) throws Exception {
 
-//        // Проверяем, что методы исходного класса калькулятора не содержат логгирование
-//        Calculator initialCalc = new CalculatorImpl();
-//        initialCalc.sum(3, 4);
-//        assertThat(output).doesNotContain("Was called method: sum() with arguments: [3, 4]");
-//
-//        initialCalc.mult(3, 4);
-//        assertThat(output).doesNotContain("Was called method: mult() with arguments: [3, 4]");
-//
-//        // Получаем бин калькулятор из контекста приложения
-//        Calculator calculatorInContext = ctx.getBean(Calculator.class);
-//
-//        // Проверяем, что при вызове его методов происходит логгирование
-//        calculatorInContext.sum(3, 4);
-//        assertThat(output).contains("Was called method: sum() with arguments: [3, 4]");
-//
-//        calculatorInContext.mult(4, 5);
-//        assertThat(output).contains("Was called method: mult() with arguments: [4, 5]");
+        // Проверяем, что методы исходного класса калькулятора не содержат логгирование
+        Calculator initialCalc = new CalculatorImpl();
+        initialCalc.sum(3, 4);
+        assertThat(output).doesNotContain("Was called method: sum() with arguments: [3, 4]");
+
+        initialCalc.mult(3, 4);
+        assertThat(output).doesNotContain("Was called method: mult() with arguments: [3, 4]");
+
+        // Получаем бин калькулятор из контекста приложения
+        Calculator calculatorInContext = ctx.getBean(Calculator.class);
+
+        // Проверяем, что при вызове его методов происходит логгирование
+        calculatorInContext.sum(3, 4);
+        assertThat(output).contains("Was called method: sum() with arguments: [3, 4]");
+
+        calculatorInContext.mult(4, 5);
+        assertThat(output).contains("Was called method: mult() with arguments: [4, 5]");
     }
 }
